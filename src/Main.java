5e7db67ebd6e44bef1738f2f2a796d00adb62622
Martin Danmo <martindanmo81gmail.com>
2025-10-07 10:24:13 +0200

    Skapar nya klass medlemmar i main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Huvudprogrammet
 public class Main
 {
+    // Main-metoden
     public static void main(String[] args)
     {
         int mynumber = 23;
         System.out.println("Hello World");
         System.out.println(mynumber);
+
+        // Skapar en ny klass
+        TeamClass classOne = new TeamClass("Class One", 10);
+        System.out.println(classOne.getName());
+        System.out.println(classOne.getPoints());
     }
 }
